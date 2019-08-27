18a01e49c1881dc9f017d82b471c5320a579696f
smg <smengar05@gmail.com>
2019-08-27 08:10:42 +0200

    0.90009 Solución de errores

--- model/Usuariojtables.go
+++ model/Usuariojtables.go
@@ -1,13 +1,13 @@
 package model
 
-// Records estructura para comunicar con jtable
+// UsuarioRecords estructura para comunicar con jtable
 type UsuarioRecords struct {
 	Result           string `json:"Resultado"`
 	TotalRecordCount int
 	Records          []Tusuario
 }
 
-// Record estructura para comunicar con jtable
+// UsuarioRecord estructura para comunicar con jtable
 type UsuarioRecord struct {
 	Result string `json:"Resultado"`
 	Record Tusuario
