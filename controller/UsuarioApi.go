2fd974e46838694de376e613a67c150c7a57d738
Fmorodia <fernandomoro77@gmail.com>
2019-08-28 11:45:59 +0200

    0.15002

--- controller/UsuarioApi.go
+++ controller/UsuarioApi.go
@@ -21,6 +21,12 @@ func Usuario(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error ", error.Error)
 	}
 }
+func Estadistica(w http.ResponseWriter, r *http.Request) {
+	error := tmpl.ExecuteTemplate(w, "estadistica", nil)
+	if error != nil {
+		fmt.Println("Error ", error.Error)
+	}
+}
 
 // UsuarioList - json con los datos de clientes
 func UsuarioList(w http.ResponseWriter, r *http.Request) {
