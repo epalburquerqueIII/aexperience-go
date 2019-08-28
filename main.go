6501ca07bf48903b21d5d2fc639fe46440d7328b
irevise <irenevise@gmail.com>
2019-08-28 11:56:24 +0200

    0.12006 main

--- main.go
+++ main.go
@@ -24,6 +24,13 @@ func main() {
 	http.Handle("/static/css/", http.StripPrefix("/static/css", http.FileServer(http.Dir("static/css"))))
 	http.Handle("/static/js/", http.StripPrefix("/static/js", http.FileServer(http.Dir("static/js"))))
 	http.Handle("/static/js/jtable", http.StripPrefix("/static/js/jtable", http.FileServer(http.Dir("static/js/jtable"))))
+	http.Handle("/static/vendor/fontawesome-free/css", http.StripPrefix("/static/vendor/fontawesome-free/css", http.FileServer(http.Dir("static/vendor/fontawesome-free/css"))))
+	http.Handle("/static/vendor/jquery-easing", http.StripPrefix("/static/vendor/jquery-easing", http.FileServer(http.Dir("/static/vendor/jquery-easing"))))
+	http.Handle("/static/vendor/chart.js", http.StripPrefix("static/vendor/chart.js", http.FileServer(http.Dir("static/vendor/chart.js"))))
+	http.Handle("/static/vendor/jquery", http.StripPrefix("/static/vendor/jquery", http.FileServer(http.Dir("/static/vendor/jquery"))))
+	http.Handle("/static/vendor/bootstrap/js", http.StripPrefix("/static/vendor/bootstrap/js", http.FileServer(http.Dir("/static/vendor/bootstrap/js"))))
+	http.Handle("/static/js/demo", http.StripPrefix("/static/js/demo/", http.FileServer(http.Dir("/static/js/demo"))))
+	http.Handle("/static/img", http.StripPrefix("/static/img", http.FileServer(http.Dir("/static/img"))))
 	http.HandleFunc("/", index)
 	// Apis usuario
 
