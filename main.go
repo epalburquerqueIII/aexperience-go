df2deca8e635eea96fc75b3ce6697c8fd99ff793
Fmorodia <fernandomoro77@gmail.com>
2019-09-12 11:23:30 +0200

    0.15008

--- main.go
+++ main.go
@@ -58,5 +58,15 @@ func main() {
 	http.HandleFunc("/autorizado/update", controller.AutorizadoUpdate)
 	http.HandleFunc("/autorizado/delete", controller.AutorizadoDelete)
 
+	// Apis reservas
+	http.HandleFunc("/reservas", controller.Reservas)
+	http.HandleFunc("/reservas/list", controller.ReservasList)
+	http.HandleFunc("/reservas/create", controller.ReservasCreate)
+	http.HandleFunc("/reservas/update", controller.ReservasUpdate)
+	http.HandleFunc("/reservas/delete", controller.ReservasDelete)
+	http.HandleFunc("/reservas/getoptionsRoles", controller.ReservasgetoptionsRoles)
+	http.HandleFunc("/reservas/getoptionsEspacios", controller.ReservasgetoptionsEspacios)
+	http.HandleFunc("/reservas/getoptionsAutorizado", controller.ReservasgetoptionsAutorizado)
+
 	http.ListenAndServe(":3000", nil)
 }
