0747302f263f3885e14cb8a081794e59b0eb53ae
jagamerog <jagg19@outlook.es>
2019-10-15 11:26:30 +0200

    0.230057 menu finalizado y funcionando

--- model/menu.go
+++ model/menu.go
@@ -2,15 +2,16 @@ package model
 
 // Tmenu es la estructura para el menú simple
 type Tmenu struct {
-	Icono        string
-	ParentTitle  string
-	Options      []Tmenudesplegable
-	uniqueOption Tmenudesplegable
-	Despliega    int
+	ID          int
+	Icono       string
+	ParentTitle string
+	Options     []Tmenudesplegable
+	Despliega   int
 }
 
 // Tmenudesplegable es la estructura para el menu desplegable
 type Tmenudesplegable struct {
 	NomEnlace string
 	Enlace    string
+	Orden     int
 }
--- util/menu usuario.go
+++ util/menu usuario.go
@@ -5,26 +5,44 @@ import (
 	"../model/database"
 )
 
+const menudesplegable = 1
+
 // Menus Estructura de menu para template
 func Menus(usertype int) []model.Tmenu {
+	var ant string = ""
+	var i int = -1
 	db := database.DbConn()
-	selDB, err := db.Query("SELECT menuparent.titulo, icono, menus.titulo, url, menuparent.tipo FROM menus LEFT OUTER JOIN menuparent ON menuparent.id = parentId")
+	selDB, err := db.Query("SELECT menuparent.id, menuparent.titulo, menus.icono, menus.orden, menus.titulo, url, menuparent.tipo FROM menus LEFT OUTER JOIN menuparent ON menuparent.id = parentId ORDER BY menuparent.id, orden")
 	if err != nil {
 		panic(err.Error())
 	}
 	menu := model.Tmenu{}
 	res := []model.Tmenu{}
 
-	desp := model.Tmenudesplegable{}
-	rst := []model.Tmenudesplegable{}
+	submenu := model.Tmenudesplegable{}
+
 	for selDB.Next() {
 
-		err = selDB.Scan(&menu.ParentTitle, &menu.Icono, &desp.NomEnlace, &desp.Enlace, &menu.Despliega)
+		err = selDB.Scan(&menu.ID, &menu.ParentTitle, &menu.Icono, &submenu.Orden, &submenu.NomEnlace, &submenu.Enlace, &menu.Despliega)
 		if err != nil {
 			panic(err.Error())
 		}
-		res = append(res, menu)
-		rst = append(rst, desp)
+		if ant != menu.ParentTitle {
+			res = append(res, menu)
+			i++
+			res[i].Options = nil
+			if menu.Despliega == menudesplegable {
+				//res[i].Options = append(res[i].Options, model.Tmenudesplegable{"HeaderMenu", ""})
+				res[i].Options = append(res[i].Options, submenu)
+			} else {
+				res[i].Options = append(res[i].Options, submenu)
+			}
+		} else {
+			if menu.Despliega == menudesplegable {
+				res[i].Options = append(res[i].Options, submenu)
+			}
+		}
+		ant = menu.ParentTitle
 	}
 	defer db.Close()
 	return res
