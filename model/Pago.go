6d7bfa8319dc047b6bd93efac6e0f6b6dfa400a7
RocioGue <rocioguerracuesta@gmail.com>
2019-10-22 13:05:23 +0200

    Revert "0.11023 cambio Fechapago por Sesiones en tabla pagos"
    This reverts commit fb47c19e89834bed99999b621df8f31d1da521e3.

--- model/Pago.go
+++ model/Pago.go
@@ -5,7 +5,7 @@ type Tpago struct {
 	Id            int64
 	IdReserva     int
 	FechaReserva  string
-	Sesiones      int
+	FechaPago     string
 	IdTipopago    int
 	TipoPago      string
 	NumeroTarjeta string
